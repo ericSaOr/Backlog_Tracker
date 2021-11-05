c263fd0cb4d0a532b815c8d7f54c770cdae383f1
Eric Sanchez <ericfsod@gmail.com>
2021-11-04 21:18:12 -0400

    added login forms, testing auth

--- backlog_tracker/src/App.js
+++ backlog_tracker/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from './Login';
 import Signup from './Signup';
 import GameContainer from './GameContainer';
@@ -10,6 +10,30 @@ import About from './About';
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
+	const [ errors, setErrors ] = useState(false);
+	const [ user, setUser ] = useState(false);
+
+	useEffect(() => {
+		fetch('/me').then((response) => {
+			if (response.ok) {
+				response.json().then((user) => setUser(user));
+			}
+		});
+	}, []);
+
+	if (!user) return <Login setUser={user} setErrors={setErrors} />;
+
+	fetch('https://rawg-video-games-database.p.rapidapi.com/games/Xenogears?key=627e46aeb3c04002ba754c38fde257d0', {
+		method: 'GET',
+		headers: {
+			'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+			'x-rapidapi-key': '07db0a293emsh92c59a3f091bfe3p173434jsn9ed539e19de8'
+		}
+	})
+		.then((response) => response.json())
+		.then((games) => console.log(games));
+
+
 	return (
 		<div>
 			<NavBar />
--- backlog_tracker/src/Login.js
+++ backlog_tracker/src/Login.js
@@ -1,15 +1,48 @@
-function Login() {
+import React, { useState } from 'react';
+
+function Login({setErrors, setUser}) {
+	const [ username, setUsername ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const [ loginErrors, setLoginErrors ] = useState([]);
+	function handleSignIn(e) {
+		e.preventDefault();
+		console.log('FIRED');
+		const user = {
+			user_name: username,
+			password
+		};
+		fetch(`/login`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(user)
+		})
+			.then((res) => res.json())
+			.then((json) => {
+				console.log('hi!');
+				if (json.error) {
+					setLoginErrors(json.error);
+				} else {
+					setErrors(false);
+				}
+			});
+		return 0;
+	}
 	return (
-		<form>
-			<h1>Login</h1>
-			<div>
-				<input type="text" name="username" placeholder="Username" />
-			</div>
-			<div>
-				<input type="password" name="password" placeholder="Password" />
-			</div>
-			<input type="submit" value="Login" />
-		</form>
+        <>
+        <h1>Please Log In!</h1>
+		<form onSubmit = {handleSignIn}>
+            <label>
+                Username
+                <input type = "text" value={username} onChange={(e)=>setUsername(e.target.value)}/>
+            </label>
+			<label>
+                Password
+                <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+            </label>
+            <input type="submit" value='Login!'/>
+			</form>
+            {/* {loginErrors?loginErrors.map(e =><div>{e}</div>):null} */}
+        </>
 	);
 }
 
